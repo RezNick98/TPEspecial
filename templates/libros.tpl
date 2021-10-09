b9018be74be2cfcb2249999cb82da99a0cf1b4b9
Maximiliano-Otharan <maxileonel.oth@gmail.com>
2021-10-09 14:37:14 -0300

    Mejoras esteticas

--- templates/libros.tpl
+++ templates/libros.tpl
@@ -14,26 +14,29 @@
         </tr>
         {/foreach}
 </table>
-<fom action="addBook" method="POST">
-    <label>Id del nuevo libro</label><input type="number" name="Id_libro">
-    <label>Titulo del libro</label><input type="text" name="Titulo">
-    <label>Genero del libro</label><select><option></option></select>
-    <label>Descripcion del libro</label><input type="text" name"Descripcion">
-    <input type="submit" name="Enviar" value="Enviar">
-</fom>
-<form action="addAutor" method="POST">
-   <label>Id del nuevo autor</label> <input type="number" name='Id_autor'>
-   <label>Nombre del autor</label> <input type="text" name='Nombre' >
-    <label>Apellido del autor</label><input type="text" name='Apellido'>
-    <input type="submit" name="Enviar" value="Enviar">
-</form>
-<form action="filterAutor" method="POST">
+
+<div class="container">
+    <fom action="addBook" method="POST">
+        <p><label>Id del nuevo libro</label> <input type="number" name="Id_libro"></p>
+        <p><label>Titulo del libro</label> <input type="text" name="Titulo"></p>
+        <p><label>Genero del libro</label> <select><option></option></select></p>
+        <p><label>Descripcion del libro</label> <input type="text" name"Descripcion"></p>
+        <p><input type="submit" name="Enviar" value="Enviar"></p>
+    </fom>
+    <form action="addAutor" method="POST"></p>
+        <p><label>Id del nuevo autor</label> <input type="number" name='Id_autor'></p>
+        <p><label>Nombre del autor</label> <input type="text" name='Nombre' ></p>
+        <p><label>Apellido del autor</label> <input type="text" name='Apellido'></p>
+        <p><input type="submit" name="Enviar" value="Enviar"></p>
+    </form>
+    <form action="filterAutor" method="POST">
         <select class="form-select mt-2" aria-label="Disabled select example">
         {foreach from=$autors item=$autor key=key name=name}    
             <option>{$autor->Apellido}, {$autor->Nombre}</option>
         {/foreach}
         </select>
         <input class="btn btn-outline-secondary mt-2" type="submit" name="Filtrar" value="Filtrar">
-</form>
+    </form>
+</div>
 
 {include file="templates/footer.tpl"}
--- templates_c/a9df112d92e87e08f3998c13fb9c6abbc94de612_0.file.libros.tpl.php
+++ templates_c/a9df112d92e87e08f3998c13fb9c6abbc94de612_0.file.libros.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-10-07 22:23:10
+/* Smarty version 3.1.39, created on 2021-10-08 23:37:43
   from 'C:\xampp\htdocs\Tpe 2\TPEspecial\templates\libros.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_615f572e1827d0_21446797',
+  'unifunc' => 'content_6160ba27496c25_05498607',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'a9df112d92e87e08f3998c13fb9c6abbc94de612' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Tpe 2\\TPEspecial\\templates\\libros.tpl',
-      1 => 1633638188,
+      1 => 1633729062,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:templates/footer.tpl' => 1,
   ),
 ),false)) {
-function content_615f572e1827d0_21446797 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6160ba27496c25_05498607 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:templates/header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
@@ -51,7 +51,21 @@ $_smarty_tpl->tpl_vars['book']->do_else = false;
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
 </table>
 
-<form action="filterAutor" method="POST">
+<div class="container">
+    <fom action="addBook" method="POST">
+        <p><label>Id del nuevo libro</label> <input type="number" name="Id_libro"></p>
+        <p><label>Titulo del libro</label> <input type="text" name="Titulo"></p>
+        <p><label>Genero del libro</label> <select><option></option></select></p>
+        <p><label>Descripcion del libro</label> <input type="text" name"Descripcion"></p>
+        <p><input type="submit" name="Enviar" value="Enviar"></p>
+    </fom>
+    <form action="addAutor" method="POST"></p>
+        <p><label>Id del nuevo autor</label> <input type="number" name='Id_autor'></p>
+        <p><label>Nombre del autor</label> <input type="text" name='Nombre' ></p>
+        <p><label>Apellido del autor</label> <input type="text" name='Apellido'></p>
+        <p><input type="submit" name="Enviar" value="Enviar"></p>
+    </form>
+    <form action="filterAutor" method="POST">
         <select class="form-select mt-2" aria-label="Disabled select example">
         <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['autors']->value, 'autor', false, 'key', 'name', array (
@@ -68,7 +82,8 @@ $_smarty_tpl->tpl_vars['autor']->do_else = false;
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
         </select>
         <input class="btn btn-outline-secondary mt-2" type="submit" name="Filtrar" value="Filtrar">
-</form>
+    </form>
+</div>
 
 <?php $_smarty_tpl->_subTemplateRender("file:templates/footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 }
